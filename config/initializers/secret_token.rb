975c8ec7b7f675ecad89c74df3d1bb3da8611f48
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-07-17 21:40:01 -0400

    Initial commit - clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopifyOrderTexter::Application.config.secret_key_base = 'ca35343fe5b16286ff6bac2ec4a0f21fd7baef6151d032af6a6a9207152ecc9cfe320c42665f522fc95f1311f8ed92ccdcfe564fa10065ef5708743fd18e2491'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShopifyOrderTexter::Application.config.session_store :cookie_store, key: '_shopify_order_texter_session'
